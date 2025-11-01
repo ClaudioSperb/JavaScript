3a54472879e1647d00a31151424f0c61534934b3
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-01 11:52:20 -0300

    atualização

--- Aual 12 EX/Ex014/script.js
+++ Aual 12 EX/Ex014/script.js
@@ -3,9 +3,9 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 10
-    msg.innerHTML = `Agora são ${hora} horas`
-
+    //var hora = 15
+  
+  msg.innerHTML = `Agora são ${hora} horas`
     if(hora >= 0 && hora < 12){
         //Bom dia !!
         img.src = 'claudio-3.png'
