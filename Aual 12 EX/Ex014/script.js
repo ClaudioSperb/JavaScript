712fc7ca765a3f2139d853b6f0ce846d15fb4ed0
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-01 10:49:28 -0300

    atualizamos o exercicio

--- Aual 12 EX/Ex014/script.js
+++ Aual 12 EX/Ex014/script.js
@@ -1,1 +1,23 @@
-alert('Olá !!!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 10
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if(hora >= 0 && hora < 12){
+        //Bom dia !!
+        img.src = 'claudio-3.png'
+        document.body.style.background ='#CBB27E'
+    } else if(hora >= 12 && hora < 18){
+        //Boa tarde !!
+        img.src = 'claudio-1.png'
+        document.body.style.background = '#A8B8A4'
+    } else{
+        //Boa noite !!
+        img.src = 'claudio-2.png'
+        document.body.style.background = '#413B41'
+    }
+}
+
