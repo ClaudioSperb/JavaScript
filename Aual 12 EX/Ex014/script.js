e29faf581acaa95986f03eac0980bce8c21c3b5b
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-03 08:20:23 -0300

    Criei comenterarios

--- Aual 12 EX/Ex014/script.js
+++ Aual 12 EX/Ex014/script.js
@@ -1,15 +1,16 @@
 function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
+    var msg = window.document.getElementById('msg')//variavel da div#msg
+    var img = window.document.getElementById('imagem')//variavel da div#imagem
+    var data = new Date()//colocamos new date para que possamos pegar a data atual
+    var hora = data.getHours()//aqui pega a hora atual
     //var hora = 15
   
-  msg.innerHTML = `Agora são ${hora} horas`
+  msg.innerHTML = `Agora são ${hora} horas`// aqui colocamos a mensagrm que vai aparecer em cima da foto, usamos um placeholder.
     if(hora >= 0 && hora < 12){
         //Bom dia !!
-        img.src = 'claudio-3.png'
-        document.body.style.background ='#CBB27E'
+        img.src = 'claudio-3.png'//puxamos a imagem da pasta
+
+        document.body.style.background ='#CBB27E'//Alteramos o background
     } else if(hora >= 12 && hora < 18){
         //Boa tarde !!
         img.src = 'claudio-1.png'
