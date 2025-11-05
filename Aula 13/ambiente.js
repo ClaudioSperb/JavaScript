3b57bdfb900dc94faa67c7a3e51e96e3913773c4
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-05 17:19:24 -0300

    Create ambiente.js

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,24 @@
+
+var c = 0
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 15)
+
+    //Nesse caso ele Testa depois, executa o codigo primeiro e depois testa a condição.
+
+
+
+
+
+
+
+
+/*
+var c = 0
+while (c <= 15) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+//Nesse caso ele testa antes, executa o codigo somente se a condição for verdadeira.
