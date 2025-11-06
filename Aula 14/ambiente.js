3ad80049eb4a0b02c6e85518e6f9da227e21dea5
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-06 15:56:34 -0300

    Create ambiente.js

---
+++ Aula 14/ambiente.js
@@ -0,0 +1,20 @@
+// Estrutura de Repetição - Laço For
+
+
+console.log('Vai começar...')
+for(var c = 1; c <= 15; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
+
+
+
+
+// Aula 14 - Ambiente de Desenvolvimento e Console
+/*
+var c = 1
+while (c <= 15) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
