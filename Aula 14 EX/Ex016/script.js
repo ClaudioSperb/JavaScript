2efc671a42c7f09519220064c04b7ab751000b64
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-08 19:48:27 -0300

    criamos o Exercicio

--- Aula 14 EX/Ex016/script.js
+++ Aula 14 EX/Ex016/script.js
@@ -1,1 +1,27 @@
-alert('Olá !!!')
+function contar(){
+    let ini = document.getElementById('txti')//inicio
+    let fim = document.getElementById('txtf')//fim
+    let passo = document.getElementById('txtp')//passo
+    let res = document.getElementById('res')//resultados
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){//verificar se os campos estão vazios
+        res.innerHTML = 'Impossível contar!'
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)//converter para número
+        let f = Number(fim.value)//converter para número
+        let p = Number(passo.value)//converter para número
+
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} \u{1F449}`//adicionar emoji
+            } 
+        } else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`//adicionar emoji
+            }
+        }
+    res.innerHTML += `\u{1F3C1}`//adicionar emoji de bandeira
+}
+}
