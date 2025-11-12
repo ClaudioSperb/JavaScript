0bb67bbea76ac3949569827dba68ab31e5ddbfef
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-12 15:27:30 -0300

    começamos o exercicio 017

--- Aula 14 EX/Ex017/script.js
+++ Aula 14 EX/Ex017/script.js
@@ -1,1 +1,9 @@
-alert('Olá !!!')
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    let n = Number(num.value)
+
+    if(num.value.length == 0){
+        alert('Por favor, digite um número válido para começarmos!')
+    }
+}
