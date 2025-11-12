e25cd3a1f8f800144321de2040878b6dec3428dd
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-12 16:28:44 -0300

    atualizamos

--- Aula 14 EX/Ex017/script.js
+++ Aula 14 EX/Ex017/script.js
@@ -2,6 +2,9 @@ function tabuada(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
     let n = Number(num.value)
+    let c = 0
+    let item = Document.createElement('option')
+    
 
     if(num.value.length == 0){
         alert('Por favor, digite um número válido para começarmos!')
