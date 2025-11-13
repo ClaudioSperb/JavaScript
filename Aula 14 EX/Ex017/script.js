279047168eb267c74ce035dd564b94f3a15d6264
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-13 16:50:20 -0300

    Update script.js

--- Aula 14 EX/Ex017/script.js
+++ Aula 14 EX/Ex017/script.js
@@ -7,10 +7,9 @@ function tabuada(){
         window.alert('Por favor, digite um número válido para começarmos!')
     }else{
         let n = Number(num.value)//converter o valor digitado em numero
-        let c = 0  //contador
+        let c = 1  //contador
         tab.innerHTML = ''   //limpar a tabuada antes de gerar uma nova
-        while (c < 10){ 
-            c++
+        for(c = 1; c < 11; c++){
             let item = document.createElement('option') //criar um elemento option
             item.text = ` ${n} x ${c} =  ${n*c}` //adicionar o texto ao elemento option
             tab.appendChild(item) //adicionar o elemento option ao select
