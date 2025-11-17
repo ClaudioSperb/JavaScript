6798e5489849b367af6d723464fb617c0bd00d45
Claudio Sperb <claudiosperbpaulo@gmail.com>
2025-11-17 10:49:38 -0300

    Update ambiente.js

--- Aula 15/ambiente.js
+++ Aula 15/ambiente.js
@@ -7,4 +7,4 @@ console.log(`Nosso vetor tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é ${num[0]}.`) */
 
 let num = [5, 8, 2, 9, 3]
-
+console.log(`O nosso vetor tem ${num.length} posições e o elemento na posição 0 é o ${num[0]}`)
